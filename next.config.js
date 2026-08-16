c6097a439ada37716af11dd47ae94578675d1ea3
Nobrain11 <chainpulseink@gmail.com>
2026-08-16 10:30:49 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  experimental: {
-    serverActions: true,
-  },
-};
-
-module.exports = nextConfig;
+"dependencies": {
+  "next": "15.5.21",
+  // ... other dependencies
+}
