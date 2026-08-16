02b1ecfc81a8b98ffc102a1128a711b9a7e7dcae
Nobrain11 <chainpulseink@gmail.com>
2026-08-16 13:39:09 +0100

    Fix module export syntax in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  // Add Webpack alias to resolve @/ to src/
   webpack: (config) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     return config;
